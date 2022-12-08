e16ffa45b0dac84d012f01e9e65f3233dc4ed75f
Mehran Mousavi <mehran.mousavi@outlook.com>
2022-12-07 17:47:44 -0800

    Delete start.sh

--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "VpnHood Server Installation for linux";
-
-sudo su -c "bash <( wget -qO- https://github.com/vpnhood/VpnHood/releases/latest/download/VpnHoodServer-linux-x64.sh)"
-sudo /opt/VpnHoodServer/vhserver gen
-sudo systemctl start VpnHoodServer
